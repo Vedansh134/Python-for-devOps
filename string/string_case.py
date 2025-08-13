27d48b3a59f8ac93f5534591f85cd0f9bf02aef3
Vedansh kumar <kumarvedansh134@gmail.com>
2025-08-13 22:17:17 +0530

    Do python string practice

---
+++ string/string_case.py
@@ -0,0 +1,7 @@
+#str = "I Love Python"
+
+str = int(input("Enter a string : "))
+
+print(str.upper())
+print(str.lower())
+
---
+++ string/string_find.py
@@ -0,0 +1,4 @@
+str = "     Python is a programming language"
+
+print(str.strip().find("is"))
+print(str.count("m"))
