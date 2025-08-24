e786c5fd711fe35f260ce5352fbfc5a576ea2dc0
Vedansh kumar <kumarvedansh134@gmail.com>
2025-08-24 12:04:01 +0530

    Practice tuple

--- ls_tuple.py/tuple.py
+++ ls_tuple.py/tuple.py
@@ -25,3 +25,29 @@ print(f"reverse order : {rev}")
 # index
 ind = tup.index(22)
 print(f"{ind}")
+
+# Concat
+t1 = (2,4,6)
+t2 = (1,3,5)
+
+concat = t1 + t2
+print(f"concatiantion : {concat}")
+
+# repeat
+repeat = t1*3
+print(repeat)
+
+# check
+print(2 in t2) # false
+
+# sliced
+print(tup[0:3]) # 0 1 2 index only not 3
+
+# min and max
+print(max(t1))
+print(min(t2))
+
+# unpacking
+tupl = ("python","js","node.js","express.js")
+a,b,c,d = tupl
+print(f"unpacking tupl :\n {a},\n {b},\n {c},\n {d}\n")
