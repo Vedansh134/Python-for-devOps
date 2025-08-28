ed43181d6e2930079eb24ba922a91d6d37c05ab9
Vedansh kumar <kumarvedansh134@gmail.com>
2025-08-28 15:42:57 +0530

    Practice dictionary

--- dictionary/dictionary_1.py
+++ dictionary/dictionary_1.py
@@ -3,7 +3,7 @@
 # key : value pairs
 
 aws_instance_info = {
-    "name" : "server",
+    "name" : "web-server",
     "id" : "564788cfgr6699",
     "type" : "t2.micro",
     "OS" : "ubuntu",
@@ -11,8 +11,8 @@ aws_instance_info = {
         "type" : "gp2",
         "size" : "30GB"
     },
-    "secruity-grps" : [22,443],
-    "script" : "apt update\n sudo apt install docker.io -y\n",
+    "security-groups" : [22,443],
+    "script" : "\n\tapt update\n\tsudo apt install docker.io -y\n",
     "a" : (1,2,3)
 }
 
@@ -23,10 +23,30 @@ except KeyError:
 except TypeError:
     print("give values() not only dict name")
 
-# get (return values)
-value = aws_instance_info.get("a")
-print(f"values : {value}")
 
-# all itms
+# get values
+val = aws_instance_info.get("name")
+print(val)
 
+# Access keys
+keys = aws_instance_info.keys()
+print(keys)
 
+# length of dictionary
+length = len(aws_instance_info)
+print(length)
+
+# Access dictionary keys and values
+for key in range(length):
+    print(f"{key} keys : {list(aws_instance_info.keys())[key]} and values : {list(aws_instance_info.values())[key]}")
+
+# Update the value
+aws_instance_info["type"] = "t3.4Xlarge"
+print(aws_instance_info)
+
+# Extend
+new_feat = {
+    "eip" : "yes"
+}
+aws_instance_info.update(new_feat)
+print(aws_instance_info)
