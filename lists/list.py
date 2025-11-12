43f906b4a010def6c5b18f360ceac7ad9cce67ce
Vedansh kumar <kumarvedansh134@gmail.com>
2025-11-12 23:22:56 +0530

    practice list in python

---
+++ lists/list.py
@@ -0,0 +1,11 @@
+# lists are like a data structure to store values
+
+list_mixed = ["fruits",True,0,100.0,False]
+
+# Use typeof keyword
+print("Type of object : ",type(list_mixed))
+
+# Access the values of list
+print(list_mixed[0:])
+print(list_mixed[:2])
+print(list_mixed[2:4])
