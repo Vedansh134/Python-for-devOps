5df2760eebf04916787dee9c45b899b7f59e7c22
Vedansh kumar <kumarvedansh134@gmail.com>
2025-11-16 20:55:14 +0530

    practice remove method in python's list

---
+++ lists/list_methods/5_remove.py
@@ -0,0 +1,9 @@
+# Remove method in list
+# Used : To remove the particular element from the list
+
+mixed_list = [2,"devOps", True,"",78,"cloud",0]
+
+mixed_list.remove(2)
+mixed_list.remove("not exist") # error
+
+print("new list : ",mixed_list)
