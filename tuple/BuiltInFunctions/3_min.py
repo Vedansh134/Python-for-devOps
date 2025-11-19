821b529a81ad990093c546f4f704f68086c67f3c
Vedansh kumar <kumarvedansh134@gmail.com>
2025-11-19 18:00:59 +0530

    practice min func in tuple

---
+++ tuple/BuiltInFunctions/3_min.py
@@ -0,0 +1,13 @@
+# min() is a Built-in function in tuple
+# Used : To get the minimum value from the tuple
+# Syntax : min(tuple)
+# Returns : Minimum value from the tuple
+
+tuple = (1,2,3,4,34,4,34,5,True)
+# Gives error if "","string"
+
+min = min(tuple)
+print(f"Minimum value in tuple : {min}")
+
+# output :
+# Minimum value in tuple : 1
