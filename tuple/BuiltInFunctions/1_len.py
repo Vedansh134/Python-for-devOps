2a7192634a96c4e03669d4cf3762b1e6a2b052bb
Vedansh kumar <kumarvedansh134@gmail.com>
2025-11-19 17:49:05 +0530

    practice len function in tuple

---
+++ tuple/BuiltInFunctions/1_len.py
@@ -0,0 +1,14 @@
+# len() is a Built-in function in tuple
+# Used : To get the length of the tuple
+# Syntax : len(tuple)
+# Returns : Integer value
+
+tuple = (2,4,6,8,(1,3,5,7),10,True,""," ","string",0,10)
+
+length = len(tuple)
+print(f"Length of the tuple : {length}")
+print(type(length))
+
+# Output:
+# Length of the tuple : 13
+# <class 'int'>
