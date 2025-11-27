171733ded0e2b84f9431ecf449b392a84dd7e5c9
Vedansh kumar <kumarvedansh134@gmail.com>
2025-11-27 19:40:55 +0530

    practice while loop

---
+++ loops/2-while_loop/1_while_loop.py
@@ -0,0 +1,12 @@
+# Basic Example: Counting Up
+
+# Initialization
+count = 0
+
+# condition run till below 5
+while count <= 5:
+    print(f"The count : {count}")
+    # Increament by +1
+    count += 1
+
+print("loop finished")
