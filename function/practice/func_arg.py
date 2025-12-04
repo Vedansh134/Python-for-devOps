1ea957aef6af65958dacdc9c244645fffd691674
Vedansh kumar <kumarvedansh134@gmail.com>
2025-12-04 19:31:49 +0530

    create sep. dir for practice & pre. codes

---
+++ function/practice/func_arg.py
@@ -0,0 +1,15 @@
+# functions in python :- It is a piece of code that we defined the code one time and run many times and each time we can also pass different arguement. No need to defined code everytime, defined code only one time and call them as your wish with different arguements!
+
+def greet(greet):
+    print(f"hello are you, Mr. {greet}")
+
+greet("Vedansh kumar")
+
+def name(name,cgpa):
+    return f"Your name : {name} and your cgpa : {cgpa} !"
+
+msg = name("vedansh",8.52)
+print(msg)
+
+
+
