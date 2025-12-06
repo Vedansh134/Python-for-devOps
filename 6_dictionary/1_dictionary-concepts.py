526813400187a612e2951d5b415c000fc51edd11
Vedansh kumar <kumarvedansh134@gmail.com>
2025-12-06 19:33:20 +0530

    practice dict concepts - get,values,keys,items

---
+++ 6_dictionary/1_dictionary-concepts.py
@@ -0,0 +1,44 @@
+# dictionay is a collection which is unordered, changeable and indexed. In Python dictionaries are written with curly brackets, and they have keys and values.
+
+# mention global variables
+Sudo="sudo"
+
+# create a dictionary
+aws_instance_info = {
+    "name" : "webserver",
+    "os" : "ubuntu-linux",
+    "image" : "ami-28075n99949328",
+    "az" : "ap-south-1",
+    "tags" : {
+        "server" : "test",
+        "env" : "testing"
+    },
+    "subnet" : "subnet-a",
+    "vpc" : "my-testvpc",
+    "ebs" : {
+        "type" : "gp2",
+        "memory" : "16gb"
+    },
+    "script" : {
+        "install-server" : "f{Sudo} yum install nginx -y\n service nginx start\n service nginx enabled\n"
+    }
+}
+
+# Access the above dictionary elements
+# get (returns None if key not found)
+value = aws_instance_info.get("name")
+print(value)
+
+# Access the items
+item = aws_instance_info.items()
+print(item)
+print("\n")
+
+# Access the keys
+keys = aws_instance_info.keys()
+print(keys)
+print("\n")
+
+# Access the values
+values = aws_instance_info.values()
+print(values)
