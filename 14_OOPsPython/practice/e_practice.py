06943953275f45c01e3b53c3f5e2a81562054da0
Vedansh kumar <kumarvedansh134@gmail.com>
2025-12-23 22:48:53 +0530

    practice inheritance

---
+++ 14_OOPsPython/practice/e_practice.py
@@ -0,0 +1,11 @@
+# inheritance practice
+
+class animals:
+    type = "heriborous"
+    @staticmethod
+    def run_fast():
+        print("some animals run fast")
+
+
+animal1 = animals()
+print(animal1.run_fast())
