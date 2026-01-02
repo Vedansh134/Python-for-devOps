1cf1118bdf7c8d4a55bc51ce4e4a4df5c014c41b
Vedansh kumar <kumarvedansh134@gmail.com>
2026-01-02 23:06:25 +0530

    learning class and object

--- 14_OOPsPython/1_class-obj-const.py
+++ 14_OOPsPython/1_class-obj-const.py
@@ -1,18 +1,37 @@
-# create class
-class Skill:
-    skills = "AWS"
-    tech = "devOps"
+# Class (The Blueprint) :
 
-    # default constructor
-    def __init__(self, name):
-        self.fname = name
-        print(self)
-        pass
+# It is a logical template.
+# It doesn't "exist" in the real world
+# It defines what properties (attributes) and actions (methods) the resource will have.
 
-    def stu(self):
-        print(f"Student name : {self.fname} and know skill : {self.skills}")
+# Object (The Instance) :
 
-# create object of a class
-s1 = Skill("Vedansh kumar")
-print(s1.skills)
-print(s1.stu())
+# It is the physical reality.
+# It is created from the class.
+# It takes up memory.
+# You can create 100 objects from just 1 class.
+
+
+# ----- defined class (The Blueprint) -----
+class S3Bucket:
+    def __init__(self, bucket_name, region, encryption=True):
+        # Attributes
+        self.bucket_name = bucket_name
+        self.region = region
+        self.encryption = encryption
+        self.files = []
+
+    # method (action)
+    def upload_files(self, file_name):
+        self.files.append(file_name)
+        print(f"{file_name} uploaded successfully to {self.bucket_name}")
+
+# ----- objects (actual objects) -----
+object_bucket = S3Bucket("web_server_123f", "ap-south-1")
+backup_bucket = S3Bucket("db_backups_34fg", "ap-south-2")
+
+print(object_bucket.encryption)
+
+# ----- Using the objects ------
+object_bucket.upload_files("ubuntu-img")
+backup_bucket.upload_files("sql_dump_jan1.zip")
